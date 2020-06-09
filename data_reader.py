aa49877c25c826bb66397cece8234514972d3e21
Anish Acharya <anishacharya@utexas.edu>
2020-06-09 13:11:32 -0500

    +

--- data_reader.py
+++ data_reader.py
@@ -1,4 +1,5 @@
 import torchvision.datasets as datasets
+from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from typing import Optional
@@ -42,31 +43,50 @@ class DataReader:
 
     def _get_mnist(self) -> [DataLoader, DataLoader, Optional[DataLoader]]:
         trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+        self.no_of_labels = 10
         mnist_train = datasets.MNIST(root=root, download=self.download, train=True, transform=trans)
-
         mnist_test = datasets.MNIST(root=root, download=self.download, train=False, transform=trans)
         test_loader = DataLoader(mnist_test)
 
         if self.dev_flag:
-            no_of_dev = self.split * mnist_train.data.shape[0]
-            no_of_train = mnist_train.data.shape[0] - no_of_dev
-            train_set, val_set = torch.utils.data.random_split(mnist_train, [no_of_train, no_of_dev])
-            train_loader = DataLoader(train_set, batch_size=self.batch_size, shuffle=True)
-            dev_loader = DataLoader(val_set, batch_size=self.batch_size, shuffle=True)
+            train_loader, dev_loader = self.split_torch_data(data_set=mnist_train, split=self.split,
+                                                             batch_size=self.batch_size)
             return train_loader, test_loader, dev_loader
 
         train_loader = DataLoader(mnist_train, batch_size=self.batch_size, shuffle=True)
-        self.no_of_labels = 10
         return train_loader, test_loader
 
     def _get_cifar10(self) -> [DataLoader, DataLoader, Optional[DataLoader]]:
         trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465),
                                                                                 (0.2023, 0.1994, 0.2010))])
+        self.no_of_labels = 10
         cifar_train = datasets.CIFAR10(root=root, download=self.download, train=True, transform=trans)
         cifar_test = datasets.CIFAR10(root=root, download=self.download, train=False, transform=trans)
-
-        train_loader = DataLoader(cifar_train, batch_size=self.batch_size, shuffle=True)
         test_loader = DataLoader(cifar_test)
 
-        self.no_of_labels = 10
+        if self.dev_flag:
+            train_loader, dev_loader = self.split_torch_data(data_set=cifar_train, split=self.split,
+                                                             batch_size=self.batch_size)
+            return train_loader, test_loader, dev_loader
+
+        train_loader = DataLoader(cifar_train, batch_size=self.batch_size, shuffle=True)
         return train_loader, test_loader
+
+    @staticmethod
+    def split_torch_data(data_set, split: float, batch_size: int) -> [DataLoader, DataLoader]:
+        # Get Number of samples in train and validation bucket based on split
+        no_of_dev = int(split * data_set.data.shape[0])
+        no_of_train = data_set.data.shape[0] - no_of_dev
+
+        # split the data set randomly into train and dev buckets and get indices
+        train_set, val_set = torch.utils.data.random_split(data_set, [no_of_train, no_of_dev])
+        train_ix = train_set.indices
+        val_ix = val_set.indices
+
+        train_sampler = SubsetRandomSampler(train_ix)
+        val_sampler = SubsetRandomSampler(val_ix)
+
+        train_loader = DataLoader(dataset=data_set, sampler=train_sampler, batch_size=batch_size)
+        val_loader = DataLoader(dataset=data_set, sampler=val_sampler, batch_size=batch_size)
+
+        return train_loader, val_loader

77993c40be7315905f73bb9e97f13b81d64176ec
Anish Acharya <anishacharya@utexas.edu>
2020-06-09 04:01:54 -0500

    +

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,8 @@
 import torchvision.datasets as datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
+from typing import Optional
+import torch
 import os
 curr_dir = os.path.dirname(__file__)
 root = os.path.join(curr_dir, './data/')
@@ -26,7 +28,6 @@ class DataReader:
         """
         self.data_set = data_set
         self.download = download
-        self.shape_of_feat = None
         self.no_of_labels = None
         self.batch_size = batch_size
         self.dev_flag = dev_set
@@ -39,17 +40,26 @@ class DataReader:
         else:
             raise NotImplementedError
 
-    def _get_mnist(self) -> [DataLoader, DataLoader]:
+    def _get_mnist(self) -> [DataLoader, DataLoader, Optional[DataLoader]]:
         trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
         mnist_train = datasets.MNIST(root=root, download=self.download, train=True, transform=trans)
+
         mnist_test = datasets.MNIST(root=root, download=self.download, train=False, transform=trans)
+        test_loader = DataLoader(mnist_test)
+
+        if self.dev_flag:
+            no_of_dev = self.split * mnist_train.data.shape[0]
+            no_of_train = mnist_train.data.shape[0] - no_of_dev
+            train_set, val_set = torch.utils.data.random_split(mnist_train, [no_of_train, no_of_dev])
+            train_loader = DataLoader(train_set, batch_size=self.batch_size, shuffle=True)
+            dev_loader = DataLoader(val_set, batch_size=self.batch_size, shuffle=True)
+            return train_loader, test_loader, dev_loader
 
         train_loader = DataLoader(mnist_train, batch_size=self.batch_size, shuffle=True)
-        test_loader = DataLoader(mnist_test)
         self.no_of_labels = 10
         return train_loader, test_loader
 
-    def _get_cifar10(self) -> [DataLoader, DataLoader]:
+    def _get_cifar10(self) -> [DataLoader, DataLoader, Optional[DataLoader]]:
         trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465),
                                                                                 (0.2023, 0.1994, 0.2010))])
         cifar_train = datasets.CIFAR10(root=root, download=self.download, train=True, transform=trans)

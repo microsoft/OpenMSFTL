5a0d863e28e7f46ffbd3ad1714acd2002d815bb3
Anish Acharya <anishacharya@utexas.edu>
2020-06-11 22:03:19 -0500

    +

--- driver.py
+++ driver.py
@@ -20,7 +20,7 @@ def _parse_args():
                         help='Training mini Batch Size')
 
     # Network Params
-    parser.add_argument('--num_clients', type=int, default=16)
+    parser.add_argument('--num_clients', type=int, default=9)
     parser.add_argument('--client_bs', type=int, default=4)
 
     args = parser.parse_args()
--- ftl/client.py
+++ ftl/client.py
@@ -1,25 +1,26 @@
 import numpy as np
-from typing import List
+from typing import List, Dict
 
 
 class Client:
     def __init__(self, client_id, adv_noise=None):
         self.client_id = client_id
+        self.local_data = None
         self.adversary_mode = adv_noise
         self.num_data_samples = 0
 
 
-def distribute_data(clients: List[Client], num_samples: int):
+def distribute_data(clients: List[Client], num_samples: int) -> Dict[int, List]:
     num_clients = len(clients)
     data_partition_ix = {}
     num_samples_per_machine = num_samples // num_clients
-    all_indexes = np.arange(num_samples)
+    all_indexes = list(np.arange(num_samples))
 
-    for ix in range(0, num_clients - 1):
-        data_partition_ix[clients[ix].client_id] = \
-            [all_indexes[num_samples_per_machine * ix: num_samples_per_machine * (ix + 1)]]
+    for ix, client in enumerate(clients):
+        data_partition_ix[client.client_id] = \
+            all_indexes[num_samples_per_machine * ix: num_samples_per_machine * (ix + 1)]
 
-    # put the rest in the last client
-    data_partition_ix[clients[-1].client_id] = [all_indexes[num_samples_per_machine * (num_clients - 1):]]
+    # append the rest in the last client
+    data_partition_ix[clients[-1].client_id].append(all_indexes[num_samples_per_machine * (num_clients - 1):])
 
     return data_partition_ix

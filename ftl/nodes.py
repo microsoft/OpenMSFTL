843091e6b7f3d50c5f5d3577a5e8e5404c26452c
Anish Acharya <anishacharya@utexas.edu>
2020-06-15 00:36:02 -0500

    +

--- ftl/nodes.py
+++ ftl/nodes.py
@@ -1,16 +1,17 @@
-import numpy as np
-from typing import List, Dict
-from torch.utils.data import DataLoader
-
-
 class Client:
     def __init__(self, client_id, adv_noise=None):
         self.client_id = client_id
         self.local_train_data = None
         self.adversary_mode = adv_noise
+        self.local_model = None
+
+    def _update_local_model(self, model):
+        self.local_model = model
 
 
 class Server:
     def __init__(self):
         self.val_loader = None
         self.test_loader = None
+        self.aggregation = None
+

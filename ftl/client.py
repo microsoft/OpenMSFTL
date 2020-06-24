e0b84c80ffbfc52cd4c3816a9feb68cb83f9970e
Anish Acharya <anishacharya@utexas.edu>
2020-06-24 18:56:15 -0500

    +

--- ftl/client.py
+++ ftl/client.py
@@ -42,10 +42,10 @@ class Client:
         w_attacked = copy.deepcopy(w)
         if type(w_attacked) == list:
             for k in range(len(w_attacked)):
-                noise = torch.randn(w[k].shape).to(torch.device("cpu")) * w_attacked[k]
+                noise = torch.randn(w[k].shape).to(device) * w_attacked[k].to(device)
                 w_attacked[k] += noise
         else:
             for k in w_attacked.keys():
-                noise = torch.randn(w[k].shape).to(torch.device("cpu")) * w_attacked[k]
+                noise = torch.randn(w[k].shape).to(device) * w_attacked[k].to(device)
                 w_attacked[k] += noise
         return w_attacked

244d73cac585e1f24cd2077301019310320c217a
Anish Acharya <anishacharya@utexas.edu>
2020-07-11 02:36:58 -0500

    +

--- plots.py
+++ plots.py
@@ -21,27 +21,33 @@ if __name__ == '__main__':
     # MNIST
     results_dir = '/mlp/'
     data = unpickle_dir(d='./results/' + data_set + results_dir)
+
+    # result_file = 'num_clients_' + str(args.num_clients) + \
+    #               '.frac_adv_' + str(args.frac_adv) + '.attack_mode_' + args.attack_mode + \
+    #               '.attack_model_' + args.attack_model + '.attack_power_' + str(args.k_std) + \
+    #               '.agg_' + args.agg + \
+    #               '.compression_' + args.compression_operator + '.bits_' + str(args.num_bits) + \
+    #               '.frac_cd_' + str(args.frac_coordinates) + '.p_' + str(args.dropout_p) + \
+    #               '.c_opt_' + args.opt + '.s_opt_' + args.server_opt
+
     # Baseline No Attack
     plot_results(result=data['num_clients_100.frac_adv_0.0.attack_mode_byzantine.attack_model_drift.attack_power_1'
                              '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
                  label='0% Byz', line_width=4)
 
     # Other
-    plot_results(result=data['num_clients_100.frac_adv_0.05.attack_mode_byzantine.attack_model_drift.attack_power_1'
-                             '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
-                 label='5% Byz pow=1', line_width=4)
     plot_results(result=data['num_clients_100.frac_adv_0.05.attack_mode_byzantine.attack_model_drift.attack_power_1.5'
                              '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
                  label='5% Byz pow=1.5', line_width=4)
-    # plot_results(result=data['num_clients_100.frac_adv_0.1.attack_mode_byzantine.attack_model_drift.attack_power_1'
-    #                          '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_optAdam'],
-    #              label='10% Byz', line_width=4)
-    # plot_results(result=data['num_clients_100.frac_adv_0.15.attack_mode_byzantine.attack_model_drift.attack_power_1'
-    #                          '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_optAdam'],
-    #              label='15% Byz', line_width=4)
-    # plot_results(result=data['num_clients_100.frac_adv_0.2.attack_mode_byzantine.attack_model_drift.attack_power_1'
-    #                          '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
-    #              label='20% Byz', line_width=4)
+    plot_results(result=data['num_clients_100.frac_adv_0.1.attack_mode_byzantine.attack_model_drift.attack_power_1.5'
+                             '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
+                 label='10% Byz pow=1.5', line_width=4)
+    plot_results(result=data['num_clients_100.frac_adv_0.15.attack_mode_byzantine.attack_model_drift.attack_power_1.5'
+                             '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
+                 label='15% Byz pow=1.5', line_width=4)
+    plot_results(result=data['num_clients_100.frac_adv_0.2.attack_mode_byzantine.attack_model_drift.attack_power_1.5'
+                             '.agg_fed_avg.compression_full.bits_2.frac_cd_0.1.p_0.1.c_opt_SGD.s_opt_Adam'],
+                 label='20% Byz pow=1.5', line_width=4)
 
     plt.title('MNIST - Convergence Plot', fontsize=14)
     plt.grid(axis='both')

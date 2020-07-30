129f6f57bda6779bb821119e174371238a2236e8
Anish Acharya <anishacharya@utexas.edu>
2020-07-29 23:09:18 -0500

    +

--- ftl/models/resnet.py
+++ ftl/models/resnet.py
@@ -1,31 +1,11 @@
 """
-Properly implemented ResNet-s for CIFAR10 as described in paper [1].
-
-The implementation and structure of this file is hugely influenced by [2]
-which is implemented for ImageNet and doesn't have option A for identity.
-Moreover, most of the implementations on the web is copy-paste from
-torchvision's resnet and has wrong number of params.
-
-Proper ResNet-s for CIFAR10 (for fair comparision and etc.) has following
-number of layers and parameters:
-
-name      | layers | params
-ResNet20  |    20  | 0.27M
-ResNet32  |    32  | 0.46M
-ResNet44  |    44  | 0.66M
-ResNet56  |    56  | 0.85M
-ResNet110 |   110  |  1.7M
-ResNet1202|  1202  | 19.4m
-
-which this implementation indeed has.
-
-Reference:
-[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
-    Deep Residual Learning for Image Recognition. arXiv:1512.03385
-[2] https://github.com/pytorch/vision/blob/master/torchvision/models/resnet.py
-
-If you use this implementation in you work, please don't forget to mention the
-author, Yerlan Idelbayev.
+Cite:
+@misc{Idelbayev18a,
+  author       = "Yerlan Idelbayev",
+  title        = "Proper {ResNet} Implementation for {CIFAR10/CIFAR100} in {PyTorch}",
+  howpublished = "\url{https://github.com/akamaster/pytorch_resnet_cifar10}",
+  note         = "Accessed: 20xx-xx-xx"
+}
 """
 
 import torch.nn as nn

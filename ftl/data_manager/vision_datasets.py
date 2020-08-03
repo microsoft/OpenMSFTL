55fb22f416aac445d1ef9737ab89a764b7735d68
Anish Acharya <anishacharya@utexas.edu>
2020-08-03 16:41:25 -0500

    +

--- ftl/data_manager/vision_datasets.py
+++ ftl/data_manager/vision_datasets.py
@@ -30,10 +30,10 @@ class FedCIFAR10(DataManager):
     def download_data(self) -> [datasets, datasets]:
         mean = [0.485, 0.456, 0.406]
         std = [0.229, 0.224, 0.225]
-        train_trans = transforms.Compose([transforms.Normalize(mean=mean, std=std),
-                                          transforms.RandomHorizontalFlip(),
+        train_trans = transforms.Compose([transforms.RandomHorizontalFlip(),
                                           transforms.RandomCrop(32, 4),
-                                          transforms.ToTensor()])
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=mean, std=std)])
         test_transform = transforms.Compose([transforms.ToTensor(),
                                              transforms.Normalize(mean=mean, std=std)])
 

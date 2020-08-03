f03de4c0c025ac6bf8196868088a9b7c5538af92
Anish Acharya <anishacharya@utexas.edu>
2020-08-03 17:55:49 -0500

    +

--- ftl/agents/server.py
+++ ftl/agents/server.py
@@ -1,17 +1,16 @@
-from .client import Client
-from ftl.training_utils.optimization import get_lr, SchedulingOptimization
-from ftl.training_utils import infer
-from ftl.models.model_helper import dist_weights_to_model, dist_grads_to_model
-from ftl.gradient_aggregation.weight_estimator import RLWeightEstimator
-from ftl.gradient_aggregation.aggregation import Aggregator
-from ftl.attacks import launch_attack
-from typing import List
-from typing import Dict
-from torch.utils.data import DataLoader
 import random
+from typing import Dict
+from typing import List
 import numpy as np
-import copy
 import torch
+from torch.utils.data import DataLoader
+from ftl.attacks import launch_attack
+from ftl.gradient_aggregation.aggregation import Aggregator
+from ftl.models.model_helper import dist_weights_to_model, dist_grads_to_model
+from ftl.training_utils import infer
+from ftl.training_utils.optimization import SchedulingOptimization
+from .client import Client
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -41,42 +40,6 @@ class Server:
         self.train_loss = []
         self.agg_grad = None
 
-        # # Aggregator tracks the model and optimizer
-        # self.aggregator = Aggregator(aggregation_config=aggregator_config,
-        #                              model=server_model,
-        #                              opt_group={'lr': self.current_lr, 'lrs': self.lrs})
-
-        # # set a weight estimator for each client gradient
-        # self.weight_estimator = None
-        # dga_config = server_opt_config.get('dga_config', None)
-        # if dga_config is not None:
-        #     dga_type = dga_config.get('type', None)
-        #     if dga_type == 'RL':
-        #         self.weight_estimator = RLWeightEstimator(dga_config)
-        #     else:
-        #         raise KeyError("Invalid gradient estimator type {}".format(dga_type))
-
-        # Server only keeps track of the pointer to the updated weights at each round
-        # self.w_current = self.aggregator.w_current
-
-    # def get_global_model(self):
-    #     """
-    #     return: global model
-    #     """
-    #     return self.aggregator.model
-    #
-    # def _update_server_lr(self):
-    #     if self.num_rounds % self.server_lr_restart == 0:
-    #         self.current_lr = self.server_lr0 / self.lr_decay
-    #         self.aggregator.set_lr(self.current_lr)
-    #
-    #     # take a step in lr_scheduler
-    #     if self.aggregator.lr_scheduler is not None:
-    #         self.aggregator.lr_scheduler.step()
-    #         self.current_lr = get_lr(self.aggregator.optimizer)
-    #
-    #     print('current lr = {}'.format(self.current_lr))
-
     def init_client_models(self):
         for client in self.clients:
             dist_weights_to_model(weights=self.w_current, parameters=client.learner.parameters())
@@ -93,30 +56,19 @@ class Server:
         # Sample Clients to Train this round
         sampled_clients = random.sample(population=self.clients, k=num_participating_client)
         epoch_loss = 0.0
-        # TODO : Parallel Calls (Defer)
-        input_feature = np.zeros(3 * num_participating_client, np.float)
-        # non-private stats used for weight aggregation
+        mal_nodes = []
         for ix, client in enumerate(sampled_clients):
             client.client_step(num_batches=client_config['num_batches'])
             epoch_loss += client.trainer.epoch_losses[-1]
-        # Modify the gradients of malicious nodes if attack is defined
-        mal_nodes = [c for c in sampled_clients if c.mal]
-        if mal_nodes:
-            launch_attack(attack_mode=attack_config["attack_mode"], mal_nodes=mal_nodes)
-        # now we can apply the compression operator before communicating to Server
-        for client in sampled_clients:
-            client.grad = client.C.compress(grad=client.grad)
-
-        # Update Metrics
+            if client.mal:
+                mal_nodes.append(client)
         train_loss = epoch_loss / len(sampled_clients)
         self.train_loss.append(train_loss.item())
 
-        # update the learning rate: self.current_lr
-        # self.num_rounds += 1
-        # self._update_server_lr()
+        # Modify the gradients of malicious nodes if attack is defined
+        if len(mal_nodes) > 0:
+            launch_attack(attack_mode=attack_config["attack_mode"], mal_nodes=mal_nodes)
 
-        # aggregate client updates
-        # self._update_global_model(sampled_clients, input_feature)
         self.agg_grad = self.aggregator.get_aggregate(clients=sampled_clients, alphas=None)
 
     def update_global_model(self):
@@ -126,34 +78,6 @@ class Server:
         self.lrs.step()
         self.w_current = np.concatenate([w.data.numpy().flatten() for w in self.learner.to('cpu').parameters()])
         dist_weights_to_model(weights=self.w_current, parameters=self.learner.to('cpu').parameters())
-
-    # def _update_global_model(self, sampled_clients, input_feature):
-    #     if self.weight_estimator is None:
-    #         self.w_current = self.aggregator.update_model(clients=sampled_clients,
-    #                                                       current_lr=self.current_lr)
-    #     else:
-    #         if self.weight_estimator.estimator_type == 'RL':
-    #             org_aggregator_state = self.aggregator.state_dict()
-    #             # run RL-based weight estimator and update a model with weighted aggregation
-    #             weights = self.weight_estimator.compute_weights(input_feature)
-    #             self.aggregator.update_model(clients=sampled_clients, current_lr=self.current_lr, alphas=weights)
-    #             rl_aggregator_state = self.aggregator.state_dict()
-    #             val_wi_rl = self.run_validation()
-    #             # aggregate without the weight
-    #             self.aggregator.load_state_dict(org_aggregator_state)  # revert to the original state
-    #             # alphas = input_feature[0:len(sampled_clients)] / np.sum(input_feature[0:len(sampled_clients)])
-    #             self.w_current = self.aggregator.update_model(clients=sampled_clients,
-    #                                                           current_lr=self.current_lr, alphas=None)
-    #             val_wo_rl = self.run_validation()
-    #             # update the RL model
-    #             should_use_rl_model = self.weight_estimator.update_model(input_feature, 1 - val_wi_rl / 100.0,
-    #                                                                      1 - val_wo_rl / 100.0)
-    #             if should_use_rl_model is True:  # keep the model updated with RL-based weighted aggregation
-    #                 self.aggregator.load_state_dict(rl_aggregator_state)
-    #                 self.w_current = self.aggregator.w_current
-    #         else:
-    #             raise NotImplementedError("Unsupported weight estimator type {}".
-    #                                       format(self.weight_estimator.estimator_type))
 
     def run_validation(self):
         """
--- ftl/gradient_aggregation/aggregation.py
+++ ftl/gradient_aggregation/aggregation.py
@@ -1,11 +1,7 @@
-from ftl.models.model_helper import dist_weights_to_model, dist_grads_to_model
-from ftl.training_utils.optimization import SchedulingOptimization
-from ftl.agents.client import Client
-from .gar import FedAvg, SpectralFedAvg
-import torch.nn as nn
 from typing import Dict, List
 import numpy as np
-import copy
+from ftl.agents.client import Client
+from .gar import FedAvg, SpectralFedAvg
 
 
 class Aggregator:
@@ -16,17 +12,6 @@ class Aggregator:
     def __init__(self, aggregation_config: Dict):
         self.aggregation_config = aggregation_config
         self.gar = self.__get_gar()
-        # # Instantiate the optimizer for an aggregator
-        # server_opt = SchedulingOptimization(model=model, opt_group=opt_group)
-        # self.optimizer = server_opt.optimizer
-        # self.lr_scheduler = server_opt.lr_scheduler
-        # self.max_grad_norm = max_grad_norm
-        # self.w_current = np.concatenate([w.data.numpy().flatten() for w in self.model.parameters()])
-
-    # def set_lr(self, current_lr):
-    #     if self.optimizer is not None:
-    #         for param_group in self.optimizer.param_groups:
-    #             param_group['lr'] = current_lr
 
     def __get_gar(self):
         if self.aggregation_config["aggregation_scheme"] == 'fed_avg':
@@ -40,85 +25,10 @@ class Aggregator:
         if len(clients) == 0:
             raise Exception('Client List is Empty')
         # create a stacked Gradient Matrix G = [G0 | G1 | .... | Gn]'
-        # each row corresponds to gradient vector for each client
+        # each row corresponds to gradient (compressed) vector for each client
         G = np.zeros((len(clients), len(clients[0].grad)), dtype=clients[0].grad.dtype)
         for ix, client in enumerate(clients):
-            G[ix, :] = client.grad
+            G[ix, :] = client.C.compress(client.grad)
         agg_grad = self.gar.aggregate(G=G, alphas=alphas)
         return agg_grad
 
-    # def update_model(self, clients: List[Client],
-    #                  current_lr: float = 0.01,
-    #                  alphas: np.ndarray = None) -> np.array:
-    #     """
-    #     Update server model with aggregated gradients
-    #     :param current_lr:
-    #     :param clients: A set of client compute nodes
-    #     :param alphas: Weight for each gradient
-    #     :return: The weights of the updated global model
-    #     """
-    #     if len(clients) == 0:
-    #         raise Exception('Client List is Empty')
-    #     # create a stacked Gradient Matrix G = [G0 | G1 | .... | Gn]'
-    #     # each row corresponds to gradient vector for each client
-    #     G = np.zeros((len(clients), len(clients[0].grad)), dtype=clients[0].grad.dtype)
-    #     for ix, client in enumerate(clients):
-    #         G[ix, :] = client.grad
-    #     agg_grad = self.gar.aggregate(G=G, alphas=alphas)
-    #
-    #     # Now Run a step of Dual Optimizer (Server Step)
-    #     self.__server_step(agg_grad=agg_grad, current_lr=current_lr)
-    #     dist_weights_to_model(weights=self.w_current, parameters=self.model.to('cpu').parameters())
-    #     return self.w_current
-
-    # def __server_step(self, agg_grad, current_lr: float):
-    #     """
-    #     Implements Server (Dual) Optimization with the option to use Adaptive Optimization
-    #     D. Dimitriadis et al., "On a Federated Approach for Training Acoustic Models ", Interspeech 2021,
-    #     S. J. Reddi et al., "Adaptive Federated Optimization", arXiv:2003.00295
-    #     """
-    #     self.set_lr(current_lr)
-    #     # Update the model with aggregated gradient
-    #     dist_grads_to_model(grads=agg_grad, parameters=self.model.to('cpu').parameters())
-    #     # apply gradient clipping
-    #     if self.max_grad_norm is not None:
-    #         grad_norm = nn.utils.clip_grad_norm_(self.model.parameters(),
-    #                                              self.max_grad_norm)
-    #     # do optimizer step
-    #     self.optimizer.step()
-    #     self.optimizer.zero_grad()
-    #
-    #     # update the model weights
-    #     self.w_current = np.concatenate([w.data.numpy().flatten() for w in self.model.parameters()])
-
-    # #####################
-    # def state_dict(self):
-    #     """Returns the state of the aggregator as a :class:`dict`.
-    #
-    #     It contains four entries:
-    #     * model_state_dict - a dict with the model state.
-    #     * w_current - a dict holding the model weights.
-    #     * optimizer_state_dict - a dict containing the optimizer state.
-    #     * lr_scheduler_state_dict - a dict keeping the LR scheduler state.
-    #     """
-    #     return {
-    #         'model_state_dict': self.model.state_dict(),
-    #         'w_current': copy.deepcopy(self.w_current),
-    #         'optimizer_state_dict': self.optimizer.state_dict() if self.optimizer is not None else None,
-    #         'lr_scheduler_state_dict': self.lr_scheduler.state_dict() if self.lr_scheduler is not None else None
-    #     }
-    #
-    # def load_state_dict(self, state_dict):
-    #     """Loads the aggregator state.
-    #
-    #     param state_dict: aggregator state. Should be an object returned from a call to :meth:`state_dict`.
-    #     """
-    #     self.model.load_state_dict(state_dict['model_state_dict'])
-    #     self.w_current = state_dict['w_current']
-    #     dist_weights_to_model(weights=self.w_current, parameters=self.model.to('cpu').parameters())
-    #
-    #     if state_dict['optimizer_state_dict'] is not None:
-    #         self.optimizer.load_state_dict(state_dict['optimizer_state_dict'])
-    #
-    #     if state_dict['lr_scheduler_state_dict'] is not None:
-    #         self.lr_scheduler.load_state_dict(state_dict['lr_scheduler_state_dict'])

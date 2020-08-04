92a721a9426321954ec463b6a893be4a5fb19d79
Anish Acharya <anishacharya@utexas.edu>
2020-08-03 20:55:04 -0500

    +

--- ftl/agents/client.py
+++ ftl/agents/client.py
@@ -40,8 +40,12 @@ class Client:
     def client_step(self):
         num_batches = self.client_opt_config.get("num_batches", 1)
         src_model_weights = np.concatenate([w.data.cpu().numpy().flatten() for w in self.learner.parameters()])
+
+        import time
+        t0 = time.time()
         for batch_ix in range(0, num_batches):
             self.trainer.train(model=self.learner)
+        print('Time to run 1 client step {}'.format(time.time()-t0))
         # compute the local gradient
         dst_model_weights = np.concatenate([w.data.cpu().numpy().flatten() for w in self.learner.parameters()])
         self.grad = src_model_weights - dst_model_weights

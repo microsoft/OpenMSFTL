633bd9d2332c4bd0382015b12a599359c252fbab
Anish Acharya <anishacharya@utexas.edu>
2020-08-04 19:21:15 -0500

    +

--- ftl/models/alexnet.py
+++ ftl/models/alexnet.py
@@ -2,29 +2,29 @@ from torch import nn
 
 
 class AlexNet(nn.Module):
-
-    def __init__(self, num_classes, num_channels):
-        super(AlexNet, self).__init__()
+    def __init__(self, num_channels=3, num_classes=10):
+        super().__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=11, stride=4, padding=2),
+            nn.Conv2d(in_channels=num_channels, out_channels=64, kernel_size=3, stride=2, padding=1),
+            nn.MaxPool2d(2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.Conv2d(64, 192, 3, padding=1),
+            nn.MaxPool2d(2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.Conv2d(192, 384, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.Conv2d(384, 256, 3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 256, 3, padding=1),
+            nn.MaxPool2d(2),
+            nn.ReLU(inplace=True)
         )
+
         self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 6 * 6, 4096),
+            nn.Dropout(0.5),
+            nn.Linear(256 * 2 * 2, 4096),
             nn.ReLU(inplace=True),
-            nn.Dropout(),
+            nn.Dropout(0.5),
             nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
             nn.Linear(4096, num_classes),
@@ -32,6 +32,6 @@ class AlexNet(nn.Module):
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.classifier(x)
-        return x
+        h = x.view(x.shape[0], -1)
+        z = self.classifier(h)
+        return z

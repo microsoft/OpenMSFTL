9f2deb167d271adeb0094d61652c9bed1f83f390
Anish Acharya <anishacharya@utexas.edu>
2020-08-05 15:52:17 -0500

    +

--- plots.py
+++ plots.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 from typing import Dict
 from matplotlib.ticker import MaxNLocator
 import matplotlib.ticker as ticker
+import json
 
 
-def plot_driver(data, label: str, result_file: str, line_width=2,
+def plot_driver(label: str, result_file: str, line_width=2,
                 plot_type: str = 'loss', ix: int = 0, marker=None, line_style=None):
-    result = data[result_file]
+    with open(result_file, 'rb') as f:
+        result = json.load(f)
     if plot_type is 'loss':
         res = result[0][0]
     elif plot_type is 'acc':
@@ -29,90 +31,16 @@ if __name__ == '__main__':
     # -------------------------------
     # ** Usually No Need to Modify **
     # -------------------------------
-    # plt.figure()
-    # fig = plt.gcf()
     ax = plt.figure().gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 
-    # MNIST
     data_set = 'mnist'
     results_dir = '/mlp/'
-    data = unpickle_dir(d='./results/' + data_set + results_dir)
-
-    # Baseline args
-    # args = {"num_clients": 100,
-    #         "frac_adv": 0.0,
-    #         "attack_mode": 'un_coordinated',
-    #         "attack_model": 'random_gaussian',
-    #         "attack_std": 1.0,
-    #         "noise_scale": 1.0,
-    #         "attack_n_std": 1.0,
-    #         "agg": 'fed_avg',
-    #         "rank": 20,
-    #         "compression_operator": 'full',
-    #         "num_bits": 2,
-    #         "frac_coordinates": 0.1,
-    #         "dropout_p": 0.1,
-    #         "c_opt": 'SGD',
-    #         "server_opt": 'Adam'}
-
-    # -----------------------------------------------
-
-    # Example Usage 1 :::
-    # --------------------------------------
-    # Plot Impact of Attacks
-    # ---------------------------------------
-    # # Baseline No Attack
-    # Specify Plot Type
-    # plot_type = 'acc'
-    # plot_driver(data=data, params=args, label='No Attack', plot_type=plot_type)
-    # # # Other
-    # args["frac_adv"] = 0.4
-    # args["attack_model"] = 'random_gaussian'
-    # args["attack_mode"] = 'un_coordinated'
-    # noise_scales = [1.0, 1.5, 2.0, 2.5, 3.0]
-    # labels = ["scale = 1", "scale = 1.5", "scale = 2", "scale = 2.5", "scale = 3"]
-    # for noise_scale, label in zip(noise_scales, labels):
-    #      args["noise_scale"] = noise_scale
-    #      plot_driver(data=data, params=args, label=label, plot_type=plot_type)
-    # plt.title('40% Uncoordinated random Gaussian Byz Noise', fontsize=14)
-    # ----------------------------------------------------------------------------------
-
-    # Example Usage :::
-    # --------------------------------------
-    # Plot Convergence (loss/ acc)
-    # ---------------------------------------
-    # # Specify Plot Type
-    # plot_type = 'acc'
-    # # plot BaseLine
-    # plot_driver(data=data, params=args, label='Fed Avg', plot_type=plot_type,
-    #             line_width=3)
-    # args["agg"] = 'fed_lr_avg'
-    # for rank in [5, 10, 15, 20, 25, 30, 35, 50]:
-    #     args["rank"] = rank
-    #     plot_driver(data=data, params=args, label='LR GAR(' + str(rank) + ')', plot_type=plot_type,
-    #                 line_width=3)
-    #
-    # ----------------------------------------------------------------------------------
-
-    # Example Usage :::
-    # --------------------------------------
-    # Plot SV Distribution
-    # ---------------------------------------
-    # Specify Plot Type
-    # plot_type = 'spectral'
-    # plt.title('Singular Value Distribution', fontsize=14)
-    #
-    # args["agg"] = 'fed_lr_avg'
-    # args["rank"] = 50
-    # # plot_driver(data=data, params=args, label='Client: SGD, Server: Adam', plot_type=plot_type)
-    # # args["c_opt"] = 'Adam'
-    # for ix in [1, 20, 50, 75, 100, 150, 200]:
-    #     label = 'Comm Round = ' + str(ix)
-    #     plot_driver(data=data, params=args, label=label, plot_type=plot_type, ix=ix-1,
-    #                 marker='*', line_width=2, line_style='--')
-    #
-    # ----------------------------------------------------------------------------------
+    o = ['result_default.pickle']
+    plot_type = 'loss'
+    for op in o:
+        result_file = './result_dumps/' + data_set + results_dir + op
+        plot_driver(label="test", result_file=result_file, plot_type=plot_type)
 
     # -------------------------------
     # ** Usually No Need to Modify **

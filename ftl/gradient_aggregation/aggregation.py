Cleaned up input feature for aggregation. Fixed a bug in Softmax-based DGA config.

ftl/gradient_aggregation/aggregation.py
@@ -12,7 +12,7 @@ from ftl.agents.client import Client
 from ftl.training_utils import infer
 from ftl.models.model_helper import dist_weights_to_model, dist_grads_to_model
 from ftl.gradient_aggregation.weight_estimator import RLWeightEstimator, SoftmaxLWeightEstimator
-from .gar import FedAvg, SpectralFedAvg, MinLoss
+from .gar import FedAvg, SpectralFedAvg
 from sklearn.utils.extmath import randomized_svd
 
 
@@ -45,12 +45,10 @@ class Aggregator:
             return FedAvg(aggregation_config=self.aggregation_config)
         elif self.aggregation_config["aggregation_scheme"] == 'fed_spectral_avg':
             return SpectralFedAvg(aggregation_config=self.aggregation_config)
-        elif self.aggregation_config["aggregation_scheme"] == 'min_loss':
-            return MinLoss(aggregation_config=self.aggregation_config)
         else:
             raise NotImplementedError
 
-    def aggregate_grads(self, clients: List[Client], client_losses: List[float], input_feature: np.ndarray=None, val_loader: DataLoader=None) -> np.array:
+    def aggregate_grads(self, clients: List[Client], input_feature: np.ndarray=None, val_loader: DataLoader=None) -> np.array:
         """
         Aggregate gradient information from clients
         """
@@ -65,7 +63,7 @@ class Aggregator:
             self.gar.Sigma_tracked.append(S)
         self.curr_G = G
         client_ids = np.array([c.client_id for c in clients])
-        self.agg_grad = self.gar.aggregate(G=G, client_ids=client_ids, losses=client_losses)
+        self.agg_grad = self.gar.aggregate(G=G, client_ids=client_ids)
 
     def update_model(self):
         """
@@ -147,7 +145,6 @@ class DGAggregator(Aggregator):
 
 
     def aggregate_grads(self, clients: List[Client],
-                              client_losses: List[float],
                               input_feature: np.ndarray,
                               val_loader: DataLoader):
         """
@@ -155,13 +152,13 @@ class DGAggregator(Aggregator):
         """
         if self.weight_estimator.estimator_type == 'softmax':
             self.gar.gradient_weights = self.weight_estimator.compute_weights(input_feature, len(clients))
-            Aggregator.aggregate_grads(self, clients=clients, client_losses=client_losses)
+            Aggregator.aggregate_grads(self, clients=clients)
             self.update_model()
         elif self.weight_estimator.estimator_type == 'RL':
             org_aggregator_state = copy.deepcopy(self.state_dict())
             # run RL-based weight estimator and update a model with weighted aggregation
             self.gar.gradient_weights = self.weight_estimator.compute_weights(input_feature)  # set the weight vector for each gradient
-            Aggregator.aggregate_grads(self, clients=clients, client_losses=client_losses)
+            Aggregator.aggregate_grads(self, clients=clients)
             self.update_model()
             rl_aggregator_state = self.state_dict()
             val_wi_rl = infer(test_loader=val_loader, model=self.model)
@@ -172,7 +169,7 @@ class DGAggregator(Aggregator):
                 # perform softmax-weighting
                 self.load_state_dict(org_aggregator_state)  # revert to the original state
                 self.gar.gradient_weights = self.sub_weight_estimator.compute_weights(input_feature, len(clients))
-                Aggregator.aggregate_grads(self, clients=clients, client_losses=client_losses)
+                Aggregator.aggregate_grads(self, clients=clients)
                 self.update_model()
                 val_wo_rl = infer(test_loader=val_loader, model=self.model)
                 print('Acc with Softmax DGA: {}'.format(val_wo_rl))
@@ -180,7 +177,7 @@ class DGAggregator(Aggregator):
                 # aggregate without the weight
                 self.load_state_dict(org_aggregator_state)  # revert to the original state
                 self.gar.gradient_weights = None  # will use a uniform weight
-                Aggregator.aggregate_grads(self, clients=clients, client_losses=client_losses)
+                Aggregator.aggregate_grads(self, clients=clients)
                 self.update_model()
                 val_wo_rl = infer(test_loader=val_loader, model=self.model)
                 print('Acc wo DGA: {}'.format(val_wo_rl))

ftl/gradient_aggregation/gar.py
@@ -20,8 +20,7 @@ class GAR:
         self.alpha_tracked = []
 
     def aggregate(self, G: np.ndarray,
-                  client_ids: np.ndarray,
-                  losses: List[float]) -> np.ndarray:
+                  client_ids: np.ndarray) -> np.ndarray:
         pass
 
     def weighted_average(self, stacked_grad: np.ndarray):
@@ -46,23 +45,9 @@ class FedAvg(GAR):
         GAR.__init__(self, aggregation_config=aggregation_config)
 
     def aggregate(self, G: np.ndarray,
-                  client_ids: np.ndarray = None,
-                  losses: List[float] = None) -> np.ndarray:
+                  client_ids: np.ndarray = None) -> np.ndarray:
         agg_grad = self.weighted_average(stacked_grad=G)
         return agg_grad
-
-
-class MinLoss(GAR):
-    def __init__(self, aggregation_config):
-        GAR.__init__(self, aggregation_config=aggregation_config)
-
-    def aggregate(self, G: np.ndarray,
-                  losses: List[float],
-                  client_ids: np.ndarray = None) -> np.ndarray:
-        if not losses:
-            raise Exception("To use MinLoss GAR , you must provide losses to aggregate call")
-        min_loss_ix = losses.index(min(losses))
-        return G[min_loss_ix, :]
 
 
 class SpectralFedAvg(GAR):
@@ -79,8 +64,7 @@ class SpectralFedAvg(GAR):
         self.auto_encoder_loss = self.aggregation_config.get("auto_encoder_loss", "scaled_mse")
 
     def aggregate(self, G: np.ndarray,
-                  client_ids: np.ndarray,
-                  losses:  List[float] = None) -> np.ndarray:
+                  client_ids: np.ndarray) -> np.ndarray:
         if self.analytic:
             # Perform Analytic Randomized PCA
             G_approx, S = fast_lr_decomposition(X=G,
@@ -116,8 +100,7 @@ class Krum(GAR):
         GAR.__init__(self, aggregation_config=aggregation_config)
 
     def aggregate(self, G: np.ndarray,
-                  client_ids: np.ndarray = None,
-                  losses: List[float] = None) -> np.ndarray:
+                  client_ids: np.ndarray = None) -> np.ndarray:
         dist = self.get_krum_dist(G=G)
         m = int(self.aggregation_config.get("krum_frac", 0.3) * G.shape[0])
         min_score = 1e10